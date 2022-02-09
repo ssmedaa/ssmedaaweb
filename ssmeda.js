2235b00bad93f76200c5186efbf6034726b1413d
ssmedaa <47162426+ssmedaa@users.noreply.github.com>
2022-02-09 00:19:01 -0500

    Add files via upload

---
+++ ssmeda.js
@@ -0,0 +1,11 @@
+function AboutFunction() {
+    location.replace(href = "about.html")
+}
+
+function ProjectFunction() {
+    location.replace(href = "projects.html")
+}
+
+function ContactFunction() {
+    location.replace(href = "contacts.html")
+}
